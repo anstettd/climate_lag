9ca09b5722890fbc5e9cb3edfcd8a2ce09ca4c27
danstett <daniel_anstett@yahoo.ca>
2022-11-18 15:51:43 -0800

    revised figs 1

--- Scripts/4.5_graphs.R
+++ Scripts/4.5_graphs.R
@@ -25,10 +25,14 @@ theme_paper3 <- function(){
     theme( 
       axis.text.x = element_text(size=12, face="bold", angle=0,hjust=0.5),
       axis.text.y = element_text(size=15,face="bold"),
-      axis.title.x = element_text(color="black", size=20, vjust = 0.5, face="bold"),
-      axis.title.y = element_text(color="black", size=20,vjust = 2, face="bold",hjust=0.5)
+      axis.title.x = element_text(color="black", size=20, vjust = 0, face="bold"),
+      axis.title.y = element_text(color="black", size=20, vjust = 1.6,face="bold",hjust=0.5 ,angle=90)
       )
 }
+
+visreg(nw_sla_lag0,"MAPA_lag0",gg=T)+ geom_point(size=0.8)+ geom_smooth(method="lm",color="black")+
+  scale_y_continuous(name="SLA")+ scale_x_continuous(name="MAPA lag 0")+  theme_paper3()   
+
 
 
 ##########################################################################################################
@@ -48,68 +52,90 @@ south_dry <- filter(y9, Region=="South" & Drought=="D")
 #Set up models
 
 #North Wet
-nw_sla_lag0 <- lmer(SLA ~  MAPA_lag0 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=north_wet)
-#nw_sla_lag0 <- lmer(SLA ~  MAPA_lag0 + (1|Block), data=north_wet)
-nw_fd_lag0 <- lmer(Experiment_Date ~  MAPA_lag0 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=north_wet)
-nw_fd_lag1 <- lmer(Experiment_Date ~  MAPA_lag1 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=north_wet)
-nw_fd_lag2 <- lmer(Experiment_Date ~  MAPA_lag2 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=north_wet)
+nw_sla_lag0 <- lmer(SLA ~  MAPA_lag0 + Block + (1|Year) + (1|Site_Lat/Family), data=north_wet)
+nw_fd_lag0 <- lmer(Experiment_Date ~  MAPA_lag0 + Block + (1|Year) + (1|Site_Lat/Family), data=north_wet)
+nw_fd_lag1 <- lmer(Experiment_Date ~  MAPA_lag1 + Block + (1|Year) + (1|Site_Lat/Family), data=north_wet)
+nw_fd_lag2 <- lmer(Experiment_Date ~  MAPA_lag2 + Block + (1|Year) + (1|Site_Lat/Family), data=north_wet)
 
 #North Dry
-nd_sla_lag0 <- lmer(SLA ~  MAPA_lag2 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=north_dry)
-nd_fd_lag0 <- lmer(Experiment_Date ~  MAPA_lag0 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=north_dry)
-nd_fd_lag1 <- lmer(Experiment_Date ~  MAPA_lag1 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=north_dry)
-nd_fd_lag2 <- lmer(Experiment_Date ~  MAPA_lag2 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=north_dry)
-nd_fd_lag01 <- lmer(Experiment_Date ~  MAPA_lag01 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=north_dry)
-nd_fd_lag012 <- lmer(Experiment_Date ~  MAPA_lag012 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=north_dry)
+nd_sla_lag0 <- lmer(SLA ~  MAPA_lag2 + Block + (1|Year) + (1|Site_Lat/Family), data=north_dry)
+nd_fd_lag0 <- lmer(Experiment_Date ~  MAPA_lag0 + Block + (1|Year) + (1|Site_Lat/Family), data=north_dry)
+nd_fd_lag1 <- lmer(Experiment_Date ~  MAPA_lag1 + Block + (1|Year) + (1|Site_Lat/Family), data=north_dry)
+nd_fd_lag2 <- lmer(Experiment_Date ~  MAPA_lag2 + Block + (1|Year) + (1|Site_Lat/Family), data=north_dry)
+nd_fd_lag01 <- lmer(Experiment_Date ~  MAPA_lag01 + Block + (1|Year) + (1|Site_Lat/Family), data=north_dry)
+nd_fd_lag012 <- lmer(Experiment_Date ~  MAPA_lag012 + Block + (1|Year) + (1|Site_Lat/Family), data=north_dry)
 
 
 #Centre Wet
-cw_sla_lag0 <- lmer(SLA ~  MAPA_lag0 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=centre_wet)
-cw_sla_lag1 <- lmer(SLA ~  MAPA_lag1 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=centre_wet)
-cw_sla_lag2 <- lmer(SLA ~  MAPA_lag2 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=centre_wet)
-cw_sla_lag012 <- lmer(SLA ~  MAPA_lag012 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=centre_wet)
+cw_sla_lag0 <- lmer(SLA ~  MAPA_lag0 + Block + (1|Year) + (1|Site_Lat/Family), data=centre_wet)
+cw_sla_lag1 <- lmer(SLA ~  MAPA_lag1 + Block + (1|Year) + (1|Site_Lat/Family), data=centre_wet)
+cw_sla_lag2 <- lmer(SLA ~  MAPA_lag2 + Block + (1|Year) + (1|Site_Lat/Family), data=centre_wet)
+cw_sla_lag012 <- lmer(SLA ~  MAPA_lag012 + Block + (1|Year) + (1|Site_Lat/Family), data=centre_wet)
 
-cw_fd_lag0 <- lmer(Experiment_Date ~  MAPA_lag0 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=centre_wet)
-cw_fd_lag1 <- lmer(Experiment_Date ~  MAPA_lag1 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=centre_wet)
-cw_fd_lag2 <- lmer(Experiment_Date ~  MAPA_lag2 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=centre_wet)
-cw_fd_lag01 <- lmer(Experiment_Date ~  MAPA_lag01 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=centre_wet)
-cw_fd_lag012 <- lmer(Experiment_Date ~  MAPA_lag012 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=centre_wet)
+cw_fd_lag0 <- lmer(Experiment_Date ~  MAPA_lag0 + Block + (1|Year) + (1|Site_Lat/Family), data=centre_wet)
+cw_fd_lag1 <- lmer(Experiment_Date ~  MAPA_lag1 + Block + (1|Year) + (1|Site_Lat/Family), data=centre_wet)
+cw_fd_lag2 <- lmer(Experiment_Date ~  MAPA_lag2 + Block + (1|Year) + (1|Site_Lat/Family), data=centre_wet)
+cw_fd_lag01 <- lmer(Experiment_Date ~  MAPA_lag01 + Block + (1|Year) + (1|Site_Lat/Family),
+                    control=lmerControl(optimizer = "bobyqa", optCtrl=list(maxfun=100000)), data=centre_wet)
+cw_fd_lag012 <- lmer(Experiment_Date ~  MAPA_lag012 + Block + (1|Year) + (1|Site_Lat/Family), data=centre_wet)
 
 #Centre Dry
-cd_sla_lag0 <- lmer(SLA ~  MAPA_lag0 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=centre_dry)
-cd_sla_lag1 <- lmer(SLA ~  MAPA_lag1 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=centre_dry)
-cd_sla_lag2 <- lmer(SLA ~  MAPA_lag2 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=centre_dry)
-cd_fd_lag0 <- lmer(Experiment_Date ~  MAPA_lag0 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=centre_dry)
-cd_fd_lag2 <- lmer(Experiment_Date ~  MAPA_lag2 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=centre_dry)
+cd_sla_lag0 <- lmer(SLA ~  MAPA_lag0 + Block + (1|Year) + (1|Site_Lat/Family), data=centre_dry)
+cd_sla_lag1 <- lmer(SLA ~  MAPA_lag1 + Block + (1|Year) + (1|Site_Lat/Family), data=centre_dry)
+cd_sla_lag2 <- lmer(SLA ~  MAPA_lag2 + Block + (1|Year) + (1|Site_Lat/Family), data=centre_dry)
+cd_fd_lag0 <- lmer(Experiment_Date ~  MAPA_lag0 + Block + (1|Year) + (1|Site_Lat/Family), data=centre_dry)
+cd_fd_lag2 <- lmer(Experiment_Date ~  MAPA_lag2 + Block + (1|Year) + (1|Site_Lat/Family), data=centre_dry)
 
 #South Wet
-cw_sla_lag1 <- lmer(SLA ~  MAPA_lag1 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=south_wet)
-cw_sla_lag2 <- lmer(SLA ~  MAPA_lag2 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=south_wet)
-
-cw_sla_lag012 <- lmer(SLA ~  MAPA_lag012 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=south_wet)
+cw_sla_lag1 <- lmer(SLA ~  MAPA_lag1 + Block + (1|Year) + (1|Site_Lat/Family), data=south_wet)
+cw_sla_lag2 <- lmer(SLA ~  MAPA_lag2 + Block + (1|Year) + (1|Site_Lat/Family), data=south_wet)
+cw_sla_lag2 <- lmer(SLA ~  MAPA_lag2 + Block + (1|Year) + (1|Site_Lat/Family), data=south_wet)
+cw_sla_lag012 <- lmer(SLA ~  MAPA_lag012 + Block + (1|Year) + (1|Site_Lat/Family), data=south_wet)
 
 #South Dry
-sd_sla_lag0 <- lmer(SLA ~  MAPA_lag0 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=south_dry)
-sd_sla_lag1 <- lmer(SLA ~  MAPA_lag1 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=south_dry)
-sd_sla_lag2 <- lmer(SLA ~  MAPA_lag2 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=south_dry)
-sd_sla_lag01 <- lmer(SLA ~  MAPA_lag01 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=south_dry)
+sd_sla_lag0 <- lmer(SLA ~  MAPA_lag0 + Block + (1|Year) + (1|Site_Lat/Family), data=south_dry)
+sd_sla_lag1 <- lmer(SLA ~  MAPA_lag1 + Block + (1|Year) + (1|Site_Lat/Family), data=south_dry)
+sd_sla_lag2 <- lmer(SLA ~  MAPA_lag2 + Block + (1|Year) + (1|Site_Lat/Family), data=south_dry)
+sd_sla_lag01 <- lmer(SLA ~  MAPA_lag01 + Block + (1|Year) + (1|Site_Lat/Family), data=south_dry)
 
-sd_sla_lag0_MATA <- lmer(SLA ~  MATA_lag0 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=south_dry)
+sd_sla_lag0_MATA <- lmer(SLA ~  MATA_lag0 + Block + (1|Year) + (1|Site_Lat/Family), data=south_dry)
 
-sd_fd_lag0 <- lmer(Experiment_Date ~  MAPA_lag0 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=south_dry)
-sd_fd_lag1 <- lmer(Experiment_Date ~  MAPA_lag1 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=south_dry)
-sd_fd_lag2 <- lmer(Experiment_Date ~  MAPA_lag2 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=south_dry)
-sd_fd_lag01 <- lmer(Experiment_Date ~  MAPA_lag01 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=south_dry)
-sd_fd_lag012 <- lmer(Experiment_Date ~  MAPA_lag012 + (1|Block) + (1|Year) + (1|Site_Lat/Family), data=south_dry)
+sd_fd_lag0 <- lmer(Experiment_Date ~  MAPA_lag0 + Block + (1|Year) + (1|Site_Lat/Family), data=south_dry)
+sd_fd_lag1 <- lmer(Experiment_Date ~  MAPA_lag1 + Block + (1|Year) + (1|Site_Lat/Family), data=south_dry)
+sd_fd_lag2 <- lmer(Experiment_Date ~  MAPA_lag2 + Block + (1|Year) + (1|Site_Lat/Family), data=south_dry)
+sd_fd_lag01 <- lmer(Experiment_Date ~  MAPA_lag01 + Block + (1|Year) + (1|Site_Lat/Family), data=south_dry)
+sd_fd_lag012 <- lmer(Experiment_Date ~  MAPA_lag012 + Block + (1|Year) + (1|Site_Lat/Family), data=south_dry)
 
 #################################################################################################################
 
 #Set up graphs
-visreg(nw_sla_lag0,"MAPA_lag0",gg=T)+ geom_point(size=0.8)+ geom_smooth(method="lm",color="black")+
-  scale_y_continuous(name="SLA")+ scale_x_continuous(name="MAPA lag 0")+ theme_paper3()
+
+#North Wet
+graph_1 <- visreg(nw_sla_lag0,"MAPA_lag0",gg=T)+ geom_point(size=0.8)+ geom_smooth(method="lm",color="black")+
+  scale_y_continuous(name="SLA")+ scale_x_continuous(name="MAPA lag 0")+  theme_paper3() 
+graph_2 <- visreg(nw_fd_lag0,"MAPA_lag0",gg=T)+ geom_point(size=0.8)+ geom_smooth(method="lm",color="black")+
+  scale_y_continuous(name="Date of Flowering")+ scale_x_continuous(name="MAPA lag 0")+  theme_paper3() 
+graph_3 <- visreg(nw_fd_lag1,"MAPA_lag1",gg=T)+ geom_point(size=0.8)+ geom_smooth(method="lm",color="black")+
+  scale_y_continuous(name="Date of Flowering")+ scale_x_continuous(name="MAPA lag 1")+  theme_paper3() 
+graph_4 <- visreg(nw_fd_lag2,"MAPA_lag2",gg=T)+ geom_point(size=0.8)+ geom_smooth(method="lm",color="black")+
+  scale_y_continuous(name="Date of Flowering")+ scale_x_continuous(name="MAPA lag 2")+  theme_paper3() 
+
+
+#North Dry
+nd_sla_lag0 <- lmer(SLA ~  MAPA_lag2 + Block + (1|Year) + (1|Site_Lat/Family), data=north_dry)
+nd_fd_lag0 <- lmer(Experiment_Date ~  MAPA_lag0 + Block + (1|Year) + (1|Site_Lat/Family), data=north_dry)
+nd_fd_lag1 <- lmer(Experiment_Date ~  MAPA_lag1 + Block + (1|Year) + (1|Site_Lat/Family), data=north_dry)
+nd_fd_lag2 <- lmer(Experiment_Date ~  MAPA_lag2 + Block + (1|Year) + (1|Site_Lat/Family), data=north_dry)
+nd_fd_lag01 <- lmer(Experiment_Date ~  MAPA_lag01 + Block + (1|Year) + (1|Site_Lat/Family), data=north_dry)
+nd_fd_lag012 <- lmer(Experiment_Date ~  MAPA_lag012 + Block + (1|Year) + (1|Site_Lat/Family), data=north_dry)
+  
 
 
 
+ggsave("Revised_figures/01_nw_sla_lag0.pdf",graph_1,width=8, height = 6, units = "in")
+ggsave("Revised_figures/02_nw_fd_lag0.pdf",graph_2,width=8, height = 6, units = "in")
+ggsave("Revised_figures/03_nw_fd_lag1.pdf",graph_3,width=8, height = 6, units = "in")
+ggsave("Revised_figures/04_nw_fd_lag2.pdf",graph_4,width=8, height = 6, units = "in")
 
 
 
