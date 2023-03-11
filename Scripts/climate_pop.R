dd6dd63ef808b6152a01de42718732e0a7d678fe
danstett <daniel_anstett@yahoo.ca>
2023-03-11 18:54:26 -0500

    pop_clim_final

--- Scripts/climate_pop.R
+++ Scripts/climate_pop.R
@@ -38,4 +38,35 @@ weather_pop <-rbind(weather8,weather9,weather_input)
 write.csv(weather_pop,'Data/weather_pop.csv') #Export file
 
 
+#log MAPA
+weather_pop$MAP.weath <- log(weather_pop$MAP.weath)
+climate$MAP <- log(climate$MAP)
+
+w_temp_2008 <- weather_pop %>% filter(Year==2008) %>% select("MAT.weath","MAP.weath","CMD.weath")
+w_temp_2009 <- weather_pop %>% filter(Year==2009) %>% select("MAT.weath","MAP.weath","CMD.weath")
+w_temp_2010 <- weather_pop %>% filter(Year==2010) %>% select("MAT.weath","MAP.weath","CMD.weath")
+w_temp_2011 <- weather_pop %>% filter(Year==2011) %>% select("MAT.weath","MAP.weath","CMD.weath")
+w_temp_2012 <- weather_pop %>% filter(Year==2012) %>% select("MAT.weath","MAP.weath","CMD.weath")
+w_temp_2013 <- weather_pop %>% filter(Year==2013) %>% select("MAT.weath","MAP.weath","CMD.weath")
+w_temp_2014 <- weather_pop %>% filter(Year==2014) %>% select("MAT.weath","MAP.weath","CMD.weath")
+w_temp_2015 <- weather_pop %>% filter(Year==2015) %>% select("MAT.weath","MAP.weath","CMD.weath")
+w_temp_2016 <- weather_pop %>% filter(Year==2016) %>% select("MAT.weath","MAP.weath","CMD.weath")
+
+
+anom2008 <- w_temp_2008 - climate[,5:7]
+anom2009 <- w_temp_2009 - climate[,5:7]
+anom2010 <- w_temp_2010 - climate[,5:7]
+anom2011 <- w_temp_2011 - climate[,5:7]
+anom2012 <- w_temp_2012 - climate[,5:7]
+anom2013 <- w_temp_2013 - climate[,5:7]
+anom2014 <- w_temp_2014 - climate[,5:7]
+anom2015 <- w_temp_2015 - climate[,5:7]
+anom2016 <- w_temp_2016 - climate[,5:7]
+
+anom <- rbind(anom2008,anom2009,anom2010,anom2011,anom2012,anom2013,anom2014,anom2015,anom2016)
+colnames(anom) <- c("MAT.anom","MAP.anom","CMD.anom")
+
+final_df <- cbind(weather_pop,anom)
+
+write.csv(final_df,'Data/pop_clim_final.csv') #Export file
 
